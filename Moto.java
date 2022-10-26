917993071e998a40a74b85528a78fe5bd83d3e34
AndriuszzZ <110136868+AndriuszzZ@users.noreply.github.com>
2022-10-26 10:58:40 -0300

    add comandos

--- Moto.java
+++ Moto.java
@@ -1,1 +1,63 @@
+import java.util.ArrayList;
 
+public class Moto extends Veiculo {
+
+    private String placa;
+    private String cor;
+    
+    private static ArrayList<Moto> motos = new ArrayList<>();
+
+    public Moto(
+        int id,
+        String nome,
+        String placa, 
+        String cor
+    ) {
+        super(id, nome);
+        this.placa = placa;
+        this.cor = cor;
+
+        motos.add(this);
+    }
+
+    public String placa() {
+        return placa;
+    } 
+
+    public void placa(String placa) {
+        this.placa = placa;
+
+    }
+
+    public String cor() {
+        return cor;
+    } 
+
+    public void cor(String cor) {
+        this.cor = cor;
+
+    }
+
+    public static Moto getMoto(int id) throws Exception {
+       for (Moto moto : motos) {
+            if (moto.getId() == id) {
+                return moto;
+            }
+       }
+       throw new Exception("moto não encontrada");
+    }
+
+    public static void removeMoto(int id) throws Exception {
+        Moto moto = getMoto(id);
+        motos.remove(moto);
+    }
+
+    @Override
+    public String toString() {
+        return "ID" + super.getId() + "\n"
+        + "Nome: " + super.getNome() + "\n"
+        + "placa: " + this.getPlaca() + "\n"
+        + "Cor: " + this.getCor() + "\n";
+    }
+        
+}
