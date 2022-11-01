a6e5f98dc6b4e28b7a67dc2aac68ac32337b34d4
AndriuszzZ <110136868+AndriuszzZ@users.noreply.github.com>
2022-11-01 09:02:22 -0300

    Update Locacao.java
    add comandos locacao

--- Locacao.java
+++ Locacao.java
@@ -1,3 +1,48 @@
+import java.util.ArrayList;
+
 public class locacao {
     
+    private int id;
+    private String data;
+    
+    puplic static ArrayList<Locacao> locacao = new ArrayList<>();
+    
+    puplic Locacao(
+        int id,
+        String data
+    ){
+        this.id = id;
+        this.data = data;
+        
+        locacao.setlocacao(this);
+        
+        locacao.add(this);
+    }
+    
+    @Override
+    public String toString() {
+        return "Id: " + id + "\n"
+        + "Data: " + data + "\n";
+    }
+    puplic int getId() {
+        return this.id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getData() {
+        return this.data;
+    }
+    public void setData(String data) {
+        this.data = data;
+    }
+    
+    public static Locacao verificaId(int id) throws Exception{
+        for (Locacao locacao : locacao) {
+            if (limpeza.id == id) {
+                return limpeza;
+            }
+        }
+        throw new Exception("Limpeza não encontrada");
+    }
 }
